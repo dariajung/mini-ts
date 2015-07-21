881c10e53b61f4d5eb3ac2dc81a8aff7fcc0bf10
Daria <djj2115cu@gmail.com>
2015-07-21 13:58:40 -0700

    wip

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,92 @@
 
 import * as ts from 'typescript';
 
-class Transpiler {
+const DEBUG = true;
+
+export class Transpiler {
+	private output: Output;
+	private currentFile: ts.SourceFile;
+
   constructor() {}
 
   helloWorld() { return 'hello world'; }
+
+  emit(s: string) {
+  	this.output.emit(s);
+  }
+
+	translateProgram(program: ts.Program): {[path: string] : string} {
+  	var paths: {[path: string] : string} = {};
+	   program.getSourceFiles()
+		      .filter((sourceFile: ts.SourceFile) =>
+		                  (!sourceFile.fileName.match(/\.d\.ts$/) &&
+		                   !!sourceFile.fileName.match(/\.[jt]s$/)))
+		      .forEach((f) => paths[f.fileName] = this.translate(f));
+    return paths;
+  }
+
+	translate(sourceFile: ts.SourceFile): string {
+		this.output = new Output();
+		if (DEBUG) this.getNodeKindInfo(sourceFile);
+		ts.forEachChild(sourceFile, (node) => this.visit(node));
+		return this.output.getResult();
+	}
+
+	transpile(fileNames: string[]) {
+			var options: ts.CompilerOptions = { target: ts.ScriptTarget.ES5, module: ts.ModuleKind.CommonJS };
+			var host = ts.createCompilerHost(options);
+			var program = ts.createProgram(fileNames, options, host);
+			var fileMap: {[s: string] : boolean} = {};
+
+			fileNames.forEach((f) => fileMap[f] = true);
+
+			program.getSourceFiles()
+				.filter((sourceFile) => fileMap[sourceFile.fileName])
+		   	// Do not generate output for .d.ts files.
+	     .filter((sourceFile: ts.SourceFile) =>
+	                !sourceFile.fileName.match(/\.d\.ts$/))
+	     .forEach((f: ts.SourceFile) => {
+	      var renamedCode = this.translate(f);
+	    });
+		}
+
+	visit(node: ts.Node) {
+		switch (node.kind) {
+			case ts.SyntaxKind.ClassDeclaration:
+				console.log('CLASS DECLARATION');
+				var cd = <ts.ClassDeclaration>node;
+				console.log(cd);
+				break;
+			default:
+				console.log('Unsupported node type (' + node.kind + ') ' + (<any>ts).SyntaxKind[node.kind] + ': ' + node.getFullText());
+				break;
+		}
+	}
+
+  visitEach(nodes: ts.Node[]) { nodes.forEach((node) => this.visit(node)); }
+
+  /* DEBUGGING PURPOSES ONLY */
+	private getNodeKindInfo(sourceFile: ts.Node) {
+    ts.forEachChild(sourceFile, (node) => {
+      console.log((<any>ts).SyntaxKind[node.kind] + ': ' + node.kind);
+      this.getNodeKindInfo(node);
+	   });
+  }
+
 }
+
+class Output {
+	private result: string = '';
+
+	emit(str: string) {
+		this.result += ' ';
+	   	this.result += str;
+	}
+
+	getResult(): string {
+		return this.result;	
+	}
+}
+
+var transpiler = new Transpiler();
+transpiler.transpile([ '../../test/input/class_decl.ts' ]);
--- test/unit/main_test.ts
+++ test/unit/main_test.ts
@@ -1,8 +1,27 @@
 /// <reference path="../../typings/mocha/mocha.d.ts"/>
 /// <reference path="../../typings/chai/chai.d.ts"/>
 
+import {Transpiler} from '../../src/main';
+import * as ts from 'typescript';
 import * as assert from 'assert';
+import * as chai from 'chai';
+
+// export function expectTranslate(tsCode: Input) {
+//   var result = translateSource(tsCode);
+//   return chai.expect(result);
+// }
+
+export function translateSource(contents: string): string {
+  var transpiler = new Transpiler();
+  var options: ts.CompilerOptions = { target: ts.ScriptTarget.ES5, module: ts.ModuleKind.CommonJS };
+  var host = ts.createCompilerHost(options);
+  var program = ts.createProgram(['test.ts'], options, host);
+  var result = transpiler.translateProgram(program);
+  return result['test.ts'];
+
+}
 
 describe('Equality statement', function() {
-  it('shows that 1 equals 1', function() { assert.equal(1, 1); })
+	it('shows that 1 equals 1', function() { assert.equal(1, 1); })
 });
+
